67187d607cd4a0419593c5ff1f9437b855fb70ec
unknown <rahul.1517.bhatia@gmail.com>
2020-11-23 19:32:04 +0530

    Patch work on info component

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -1,5 +1,5 @@
 import { FaApple,FaDocker,FaAndroid,FaReact, FaPython, FaSass, FaNodeJs } from "react-icons/fa";
-import { FcAndroidOs } from "react-icons/fc";
+import { SiKotlin,SiJava,SiDigitalocean,SiGraphql,SiPytorch,SiTensorflow } from "react-icons/si";
 
 export const homeObjOne = {
   id: "Technology",
@@ -9,13 +9,19 @@ export const homeObjOne = {
   topLine: "What I Love to Do",
   headline: "Building Mobile and Web apps Having Intelligence.",
   description: [
-    <FaReact size={42} style={{ color: "#03814C" }} />,
-    <FaAndroid size={42} style={{ color: "#03814C" }}/>,
-    <FaPython size={42} style={{ color: "#03814C" }}/>,
-    <FaSass size={42} style={{ color: "#03814C" }}/>,
+    <FaAndroid onClick={()=>alert("android")}size={42} style={{ color: "#03814C" }}/>,
+    <FaApple size={42} onClick={()=>alert("Apple")}style={{ color: "#03814C" }}/>,
+    <SiJava size={42} style={{ color: "#03814C" ,margin:10}}/>,
+    <SiKotlin size={34} style={{ color: "#03814C" }}/>,
     <FaNodeJs size={42} style={{ color: "#03814C" }}/>,
+    <FaReact size={42} style={{ color: "#03814C" }} />,
+    <FaSass size={42} style={{ color: "#03814C" }}/>,
+    <SiGraphql size={42} style={{ color: "#03814C" }}/>,  
+    <FaPython size={42} style={{ color: "#03814C" }}/>,
+    <SiTensorflow size={42} style={{ color: "#03814C" }}/>,
+    <SiPytorch size={42} style={{ color: "#03814C" }}/>,
     <FaDocker size={42} style={{ color: "#03814C" }}/>,
-    <FaApple size={42} style={{ color: "#03814C" }}/>,
+    <SiDigitalocean size={42} style={{ color: "#03814C" }}/>,
   ],
   buttonLabel: "Get Quote",
   imgStart: false,
